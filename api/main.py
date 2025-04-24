834c409687b2e91020426cb2aca1524822edc36e
agunggst <rqz.agastya@gmail.com>
2025-04-24 17:58:33 +0700

    fix env fastAPI

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import os
 
 # --- Load ENV ---
-load_dotenv()
+load_dotenv(dotenv_path="../.env")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MONGO_DB_PASS = os.getenv("MONGO_DB_PASS")
 
